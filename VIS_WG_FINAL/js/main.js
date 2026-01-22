f7e3408fbf3a55ca1ba646080981dd4cf1201c1c
André Maciel Sousa <1300012@estudante.uab.pt>
2026-01-22 20:20:54 +0000

    Update main.js

--- VIS_WG_FINAL/js/main.js
+++ VIS_WG_FINAL/js/main.js
@@ -1,3 +1,4 @@
+/*
 import { loadData } from "./data.js";
 
 (async function () {
@@ -11,3 +12,25 @@ import { loadData } from "./data.js";
     distritosCSV: data.districtIds.length
   });
 })();
+*/
+
+(async function () {
+  const url = "data/districts.topo.json";
+  const resp = await fetch(url, { cache: "no-store" });
+  console.log("Topo status:", resp.status, resp.statusText);
+
+  const topo = await resp.json();
+  console.log("Topo type:", topo.type);
+  console.log("Objects disponíveis:", Object.keys(topo.objects || {}));
+
+  const firstObjKey = Object.keys(topo.objects || {})[0];
+  console.log("Primeiro objeto:", firstObjKey);
+
+  const geo = topojson.feature(topo, topo.objects[firstObjKey]);
+  console.log("N features:", geo.features.length);
+
+  const p = geo.features[0]?.properties || {};
+  console.log("Propriedades (feature 0):", Object.keys(p));
+  console.log("Exemplo properties:", p);
+})();
+
