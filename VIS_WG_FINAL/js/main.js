a233cfa10450ce1317636094650c92ae6893aef0
André Maciel Sousa <1300012@estudante.uab.pt>
2026-01-22 20:24:49 +0000

    Update main.js

--- VIS_WG_FINAL/js/main.js
+++ VIS_WG_FINAL/js/main.js
@@ -15,21 +15,15 @@ import { loadData } from "./data.js";
 */
 
 (async function () {
-  const url = "data/districts.topo.json";
+  const url = "data/districts.topo.json"; // pode continuar com este nome
   const resp = await fetch(url, { cache: "no-store" });
-  console.log("Topo status:", resp.status, resp.statusText);
+  console.log("GeoJSON status:", resp.status, resp.statusText);
 
-  const topo = await resp.json();
-  console.log("Topo type:", topo.type);
-  console.log("Objects disponíveis:", Object.keys(topo.objects || {}));
+  const geo = await resp.json();
+  console.log("GeoJSON type:", geo.type);
+  console.log("N features:", geo.features?.length);
 
-  const firstObjKey = Object.keys(topo.objects || {})[0];
-  console.log("Primeiro objeto:", firstObjKey);
-
-  const geo = topojson.feature(topo, topo.objects[firstObjKey]);
-  console.log("N features:", geo.features.length);
-
-  const p = geo.features[0]?.properties || {};
+  const p = geo.features?.[0]?.properties || {};
   console.log("Propriedades (feature 0):", Object.keys(p));
   console.log("Exemplo properties:", p);
 })();
